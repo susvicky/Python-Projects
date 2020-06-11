cad0d92c62cd271a4c5c162fda6d9572a92f6572
susvicky <susvicky393@gmail.com>
2020-06-11 23:48:10 +0530

    adding watermark to pdf

---
+++ Adding_watermark_to_a_pdf/add_watermark.py
@@ -0,0 +1,30 @@
+import PyPDF2
+
+pdf_file = "doc.pdf"
+
+watermark = "watermark.pdf"
+
+merged_file = "merged.pdf"
+
+input_file = open(pdf_file,'rb')
+input_pdf = PyPDF2.PdfFileReader(pdf_file)
+
+watermark_file = open(watermark,'rb')
+watermark_pdf = PyPDF2.PdfFileReader(watermark_file)
+
+pdf_page = input_pdf.getPage(0)
+
+watermark_page = watermark_pdf.getPage(0)
+
+pdf_page.mergePage(watermark_page)
+
+output = PyPDF2.PdfFileWriter()
+
+output.addPage(pdf_page)
+
+merged_file = open(merged_file,'wb')
+output.write(merged_file)
+
+merged_file.close()
+watermark_file.close()
+input_file.close()
